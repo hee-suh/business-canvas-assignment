a16a5ca4786752655a5f53a0ca5a98c2218f2196
hee-suh <suhee0206@gmail.com>
2024-12-07 23:20:20 +0900

    feat: add member form modal n table to member list

--- src/views/MemberList.tsx
+++ src/views/MemberList.tsx
@@ -1,18 +1,37 @@
+import MemberFormModal from '@/features/MemberList/components/MemberFormModal';
+import MemberTable from '@/features/MemberList/components/MemberTable';
 import { PlusOutlined } from '@ant-design/icons';
-import { Button, Flex, Table, Typography } from 'antd';
+import { Button, Flex, Typography } from 'antd';
+import { useState } from 'react';
 
 export default function MemberList() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [memberId, setMemberId] = useState('');
+
+  const showModal = (id?: string) => {
+    if (id) {
+      setMemberId(id);
+    }
+    setIsModalOpen(true);
+  };
+
+  const closeFormModal = () => {
+    setMemberId('');
+    setIsModalOpen(false);
+  };
+
   return (
     <>
       <Flex justify="space-between" align="center" style={{ paddingBlock: 8, paddingInline: 14 }}>
         <Typography.Title level={5} style={{ marginTop: 0 }}>
           회원 목록
         </Typography.Title>
-        <Button color="primary" variant="solid" icon={<PlusOutlined />}>
+        <Button color="primary" variant="solid" icon={<PlusOutlined />} onClick={() => showModal()}>
           추가
         </Button>
+        <MemberFormModal id={memberId} isOpen={isModalOpen} closeModal={closeFormModal} />
       </Flex>
-      <Table />
+      <MemberTable showModal={showModal} />
     </>
   );
 }
