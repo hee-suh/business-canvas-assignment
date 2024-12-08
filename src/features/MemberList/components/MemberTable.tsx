a10c08871fea4c5770e0e18c11fe7402b5b9c362
hee-suh <suhee0206@gmail.com>
2024-12-08 15:36:24 +0900

    feat: implement member table filter

--- src/features/MemberList/components/MemberTable.tsx
+++ src/features/MemberList/components/MemberTable.tsx
@@ -1,9 +1,10 @@
+import { CheckboxGroup } from '@/components/ui/CheckboxGroup';
 import Table from '@/components/ui/Table';
 import { fields } from '@/features/MemberList/data/memberData';
 import { memberStorageOperation } from '@/features/MemberList/services/memberStorage';
 import type { MemberRecord } from '@/models/member.interface';
 import { MoreOutlined } from '@ant-design/icons';
-import type { MenuProps } from 'antd';
+import type { MenuProps, TableColumnType } from 'antd';
 import { Button, Checkbox, Dropdown } from 'antd';
 import type { TableRowSelection } from 'antd/es/table/interface';
 import type { Key } from 'react';
@@ -15,20 +16,57 @@ interface MemberTableProps {
   showModal: (id: string) => void;
 }
 
-const columns = [
-  ...fields.map((field) => ({
-    title: field.label,
-    dataIndex: field.name,
-    key: field.name,
-    render: (value: string | boolean) =>
-      field.type === 'checkbox' ? <Checkbox checked={Boolean(value)} /> : value,
-    filters: [], // TODO: filtering
-  })),
-];
-
 export default function MemberTable({ records, updateRecords, showModal }: MemberTableProps) {
   const [recordId, setRecordId] = useState('');
   const [selectedRowKeys, setSelectedRowKeys] = useState<Key[]>([]);
+
+  const getColumnFilterProps = (dataIndex: keyof MemberRecord): TableColumnType<MemberRecord> => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm }) => {
+      const uniqueOptions = [...new Set(records.map((item) => item[dataIndex]))]
+        .filter((x) => x !== undefined && x !== null && x !== '')
+        .map((option) => {
+          return {
+            label:
+              dataIndex === 'emailSubscription'
+                ? option
+                  ? '선택됨'
+                  : '선택 안함'
+                : String(option),
+            value: option,
+          };
+        });
+
+      return (
+        <CheckboxGroup
+          options={uniqueOptions}
+          value={selectedKeys as (string | boolean)[]}
+          onChange={(checkedValues) => {
+            setSelectedKeys(checkedValues as Key[]);
+            confirm();
+          }}
+        />
+      );
+    },
+    onFilter: (value, record) => {
+      const field = record[dataIndex];
+      if (dataIndex === 'emailSubscription') {
+        return value === field;
+      } else {
+        return String(field).includes(String(value));
+      }
+    },
+  });
+
+  const columns = [
+    ...fields.map((field) => ({
+      title: field.label,
+      dataIndex: field.name,
+      key: field.name,
+      render: (value: string | boolean) =>
+        field.type === 'checkbox' ? <Checkbox checked={Boolean(value)} /> : value,
+      ...getColumnFilterProps(field.name),
+    })),
+  ];
 
   const handleClickAction = (id: string) => {
     setRecordId(id);
@@ -76,6 +114,7 @@ export default function MemberTable({ records, updateRecords, showModal }: Membe
 
   return (
     <Table<MemberRecord>
+      className="member-table"
       rowSelection={rowSelection}
       columns={[
         ...columns,
